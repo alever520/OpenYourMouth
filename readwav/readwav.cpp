977e9b6dec6119d84986b691760b5b1a46b50d83
alever <qwwbijie@163.com>
2016-12-21 18:31:22 +0800

    获得了wav的数据内容，并且根据采样频率，重新设定了输出数据，大概每1ms输出一个数据

--- readwav/readwav.cpp
+++ readwav/readwav.cpp
@@ -9,7 +9,7 @@ class readwav
 	unsigned int ChunkSize;
 	char Format[4];
 	char SubChunk1ID[4];
-	unsigned int SubChunk1Size[4];
+	unsigned int SubChunk1Size;
 	unsigned short AudioFormat;
 	unsigned short NumChannels;
 	unsigned int SampleRate;
@@ -25,34 +25,36 @@ public:
 		fp.open(filename, ios::in | ios::binary);
 		fp.seekg(0, ios::beg);
 		fp.read((char*)&ChunkID, 4);//sizeof(ChunkID)
-		fp.seekg(4,ios::cur);
-		//fp.read((char*)&ChunkSize, sizeof(ChunkSize));//sizeof(ChunkSize)
-		fp.seekg(4, ios::cur);
-		fp.read((char*)&Format, sizeof(Format));//sizeof(Format)
-		fp.seekg(4, ios::cur);
-		fp.read((char*)&SubChunk1ID, sizeof(SubChunk1ID));
-		fp.seekg(4, ios::cur);
-		fp.read((char*)&SubChunk1Size, sizeof(SubChunk1Size));
-		//fp.seekg(0x12);
-		//fp.read((char*)&AudioFormat, sizeof(AudioFormat));
-		//fp.seekg(0x14);
-		//fp.read((char*)&NumChannels, sizeof(NumChannels));
-		//fp.seekg(0x18);
-		//fp.read((char*)&SampleRate, sizeof(SampleRate));
-		//fp.seekg(0x1c);
-		//fp.read((char*)&ByteRate, sizeof(ByteRate));
-		//fp.seekg(0x20);
-		//fp.read((char*)&BlockAlign, sizeof(BlockAlign));
-		//fp.seekg(0x22);
-		//fp.read((char*)&BitsPerSample, sizeof(BitsPerSample));
-		//fp.seekg(0x24);
-		//fp.read((char*)&SubChunk2ID, sizeof(SubChunk2ID));
+		fp.seekg(0x04);
+		fp.read((char*)&ChunkSize, sizeof(ChunkSize));//sizeof(ChunkSize)
+		fp.seekg(0x08);
+		fp.read((char*)&Format, 4);//sizeof(Format)
+		fp.seekg(0x0c);
+		fp.read((char*)&SubChunk1ID, 4);
+		fp.seekg(0x10);
+		fp.read((char*)&SubChunk1Size, 4);
+		fp.seekg(0x14);
+		fp.read((char*)&AudioFormat, sizeof(AudioFormat));
+		fp.seekg(0x16);
+		fp.read((char*)&NumChannels, sizeof(NumChannels));
+		fp.seekg(0x18);
+		fp.read((char*)&SampleRate, sizeof(SampleRate));
+		fp.seekg(0x1c);
+		fp.read((char*)&ByteRate, sizeof(ByteRate));
+		fp.seekg(0x20);
+		fp.read((char*)&BlockAlign, sizeof(BlockAlign));
+		fp.seekg(0x22);
+		fp.read((char*)&BitsPerSample, sizeof(BitsPerSample));
+		fp.seekg(0x24);
+		fp.read((char*)&SubChunk2ID, sizeof(SubChunk2ID));
 		fp.seekg(0x28);
 		fp.read((char*)&SubChunk2Size, sizeof(SubChunk2Size));
 		//long length = char2num(SubChunk2Size);
+		data = new unsigned char[SubChunk2Size];
 		fp.seekg(0x2c);
-		//data = new unsigned char[SubChunk2Size];
-		//fp.read((char*)&data, sizeof(data));
+		fp.read((char*)data, SubChunk2Size);
+		
+		
 
 		cout << "ChunkID " << ChunkID << endl;
 		cout << "ChunkSize " << ChunkSize << endl;
@@ -67,26 +69,41 @@ public:
 		cout << "BitsPerSample " << BitsPerSample << endl;
 		cout << "SubChunk2ID " << SubChunk2ID << endl;
 		cout << "SubChunk2Size " << SubChunk2Size << endl;
+		//cout << n << endl;
+		
 
 	}
-	//void char2num(char*p)
-	//{
-	//	while (*p != '\0')
-	//	{
-	//		if (*p >= '0' && c <= '9')
-	//			return c - '0';
-	//		else if (*p >= 'a' && c <= 'f')
-	//			return (c - 'a' + 10);
-	//		else if (*p >= 'A' && c <= 'F')
-	//			return (c - 'A' + 10);
-	//		//assert(0);
-	//		return 0;
-	//	}
-	//}
+	void disp()
+	{
+		double output = 0;
+		for (unsigned int i = 0; i < SubChunk2Size; i = i + 2)
+		{
+			unsigned long data_low = data[i];
+			unsigned long data_high = data[i + 1];
+			double data_true = data_high * 256 + data_low;
+			long data_complement = 0;
+			int my_sign = (int)(data_high / 128);
+			if (my_sign == 1)
+				data_complement = data_true - 65536;
+			else
+				data_complement = data_true;
+			double final_data = (double)(data_complement / (double)32768);
+			output = output + final_data;
+			if (i % 1764 == 0)
+			{
+				ofstream out("test.txt",ios::app);
+				out << output << endl;
+				output = 0;
+			}
+			
+		}
+	}
+	
 };
 int main()
 {
-	readwav read("test.wav");
+	readwav read("test1.wav");
+	read.disp();
 	system("pause");
 	
 }
